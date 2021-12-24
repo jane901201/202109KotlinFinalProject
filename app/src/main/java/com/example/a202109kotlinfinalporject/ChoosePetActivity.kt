785a1608b2242afbed914b75861ca1572c2284cc
jane901201 <jane36742715@gmail.com>
2021-12-24 21:14:06 +0800

    begin deal ui

---
+++ app/src/main/java/com/example/a202109kotlinfinalporject/ChoosePetActivity.kt
@@ -0,0 +1,15 @@
+package com.example.a202109kotlinfinalporject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class ChoosePetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choosepet)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/a202109kotlinfinalporject/MainMenuActivity.kt
@@ -0,0 +1,14 @@
+package com.example.a202109kotlinfinalporject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainmenu)
+
+
+        }
+}
