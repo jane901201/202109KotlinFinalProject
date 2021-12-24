0f17902916917d91e29a21010b1277cd455f94cb
jane901201 <jane36742715@gmail.com>
2021-12-24 23:27:05 +0800

    All UI TypeSetting

---
+++ app/src/main/java/com/example/a202109kotlinfinalporject/RecordsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a202109kotlinfinalporject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_records)
+    }
+}
